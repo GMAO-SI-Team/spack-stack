20db4b88fa3ee216ff87563913620af5e5a8c762
Dom Heinzeller <dom.heinzeller@icloud.com>
2025-05-21 10:22:10 -0600

    Update util/check_libirc.sh to also check executables, and add documentation in doc/source/Utilities.rst

--- util/check_libirc.sh
+++ util/check_libirc.sh
@@ -2,9 +2,20 @@
 
 ierror=0
 
-searchfor="librc.so"
+searchfor="libintlc.so"
 
+echo "Checking shared libraries for ${searchfor} ..."
 for file in `find ${SPACK_ENV} -type f -iname '*.so*'`; do
+  # Return code from grep is 1 if not found, 0 if found
+  ldd $file 2>&1 | grep "${searchfor}"
+  if [[ $? -eq 0 ]]; then
+    echo "Found ${searchfor} linked in ${file}"
+    ierror=1
+  fi
+done
+
+echo "Checking executables for ${searchfor} ..."
+for file in `find ${SPACK_ENV} -type f -executable`; do
   # Return code from grep is 1 if not found, 0 if found
   ldd $file 2>&1 | grep "${searchfor}"
   if [[ $? -eq 0 ]]; then
