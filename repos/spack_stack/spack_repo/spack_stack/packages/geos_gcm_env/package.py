cd7f0a54da95beb5e632bd2309b7bd2d2492a059
Matthew Thompson <matthew.thompson@nasa.gov>
2026-08-26 08:21:23 -0400

    Support for FMS 2026 GEOS

--- repos/spack_stack/spack_repo/spack_stack/packages/geos_gcm_env/package.py
+++ repos/spack_stack/spack_repo/spack_stack/packages/geos_gcm_env/package.py
@@ -16,11 +16,13 @@ class GeosGcmEnv(BundlePackage):
     maintainers("climbfuji", "mathomp4", "Dooruk")
 
     # Current version
+    version("12.1.0")
     version("11.10.0")
 
     variant("debug", default=False, description="Build debug version of selected dependencies")
     variant("fms", default=True, description="Build FMS as part of the environment")
     variant("cdo", default=True, description="Build CDO as part of the environment")
+    variant("grads", default=False, description="Build GrADS as part of the environment")
 
     depends_on("base-env", type="run")
 
@@ -34,7 +36,13 @@ class GeosGcmEnv(BundlePackage):
     depends_on(
         "fms@2024.03 precision=32,64 ~gfs_phys +openmp +pic constants=GEOS +deprecated_io +yaml build_type=Release", #noqa: E501
         type="run",
-        when="@11.10: +fms",
+        when="@11.10:12.0 +fms",
+    )
+
+    depends_on(
+        "fms@2026.01.01 precision=mixed ~gfs_phys +openmp +pic constants=GEOS +yaml build_type=Release", #noqa: E501
+        type="run",
+        when="@12.1: +fms",
     )
 
     # mapl is built as part of GEOS, don't load;
@@ -66,5 +74,7 @@ class GeosGcmEnv(BundlePackage):
     depends_on("nco", type="run")
     depends_on("nccmp", type="run")
     depends_on("cdo", type="run", when="+cdo")
+
+    depends_on("grads", type="run", when="+grads")
 
     # There is no need for install() since there is no code.
